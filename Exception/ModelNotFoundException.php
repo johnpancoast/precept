d35e2f18f96b1a287db48d2dfaf8d860cc4b6e9d
John Pancoast <shideon@gmail.com>
2014-10-12 17:47:34 -0700

    most of the tag user functionality

---
+++ Exception/ModelNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * No model found
+ *
+ * @copyright (c) 2014 John Pancoast
+ * @author John Pancoast <shideon@gmail.com>
+ */
+
+namespace Shideon\Bundle\SmeeApiBundle\Model\Exception;
+
+/**
+ * No model found
+ *
+ * @author John Pancoast <shideon@gmail.com>
+ */
+class ModelNotFoundException extends \Exception {}
