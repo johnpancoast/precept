0d86f415858a7c1080b4a239f24edfe50c59882e
John Pancoast <johnpancoaster@gmail.com>
2016-02-01 23:00:42 -0800

    Add beginnings of idea of models being related to identities

--- src/ModelInterface.php
+++ src/ModelInterface.php
@@ -9,7 +9,7 @@
 namespace Pancoast\Precept;
 
 use Pancoast\Precept\Model\CallableActionRegistry;
-use Pancoast\Precept\Model\CallableRegistryInterface;
+use Pancoast\Precept\Model\RepositoryInterface;
 
 /**
  * Contract for a model
@@ -19,14 +19,66 @@ use Pancoast\Precept\Model\CallableRegistryInterface;
 interface ModelInterface
 {
     /**
-     * Register actions
+     * Get repository
      *
-     * The model wrapper at {@see ModelWrapperInterface} will allow calls to registered callables. These callables are
-     * defined here by the consumer (you, in each model in your application). You register the calls you want to be
-     * accessible by calling methods on the passed registry.
-     *
-     * @param \Pancoast\Precept\Model\CallableRegistryInterface $callableRegistry
-     * @return void
+     * @return RepositoryInterface
      */
-    public function registerModelCallables(CallableRegistryInterface $callableRegistry);
+    public function getRepository();
+
+    /**
+     * Set repository
+     *
+     * The implementation of this interface should use this repository to find and persist entities.
+     *
+     * @param RepositoryInterface $repository
+     * @return self
+     */
+    public function setRepository(RepositoryInterface $repository);
+
+    /**
+     * Load model by id
+     *
+     * Implementation should use internals to find identity (most likely via the repository the interface expects).
+     *
+     * @param mixed $id Unique id
+     * @see self::setRepository()
+     */
+    public function loadIdentityById($id);
+
+    /**
+     * Get model object's serialized identity
+     *
+     * @return object Model identity
+     */
+    public function getIdentity();
+
+    /**
+     * Set the identity of the current model object
+     *
+     * @param object $identity Model identity
+     * @return self
+     */
+    public function setIdentity($identity);
+
+    /**
+     * Set identity using an unserialized data representation
+     *
+     * @param mixed $data This may be an object, array or some other data structure that the implementation decides
+     * @return self
+     */
+    public function setIdentityData($data);
+
+    /**
+     * Get unserialized identity data
+     *
+     * @return mixed This may be an object, array or some other data structure that the implementation decides
+     */
+    public function getIdentityData();
+
+    /**
+     * Delete this model object's identity
+     *
+     * @return bool Success
+     */
+    public function delete();
 }
