1313013732948c0bb275f589836e878bebb254aa
John Pancoast <johnpancoaster@gmail.com>
2016-03-17 16:21:03 -0700

    Update doc

--- src/ModelFactory.php
+++ src/ModelFactory.php
@@ -12,7 +12,8 @@ namespace Pancoast\Precept;
  * Model factories create models
  *
  * While your application can instantiate models directly it's advisable to use this instead in case model creation
- * changes internally to precept. Extending this class is still allowed in those cases.
+ * changes internally to precept. Extending this class is still allowed in those cases as long as you take internals
+ * into account (at present, nothings really happening here yet).
  *
  * @author John Pancoast <johnpancoaster@gmail.com>
  */
