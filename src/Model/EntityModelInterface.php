8f40a69c5a81e8746f79ccb77845f881025ef3e8
John Pancoast <johnpancoaster@gmail.com>
2017-02-27 10:16:55 -0800

    Remove construction method from interface
    Mistake.

--- src/Model/EntityModelInterface.php
+++ src/Model/EntityModelInterface.php
@@ -10,46 +10,30 @@ namespace Pancoast\Precept\Model;
 
 use Pancoast\Precept\Entity\EntityInterface;
 use Pancoast\Precept\Model\CallableActionRegistry;
-use Pancoast\Precept\ModelFactory\EntityModelFactoryInterface;
-use Pancoast\Precept\ObjectRegistry\RepositoryRegistryInterface;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Contract for a model that relates directly with an entity
  *
- * Models have the following characteristics:
+ * This is not doing much and is mainly for type-hinting. You might want to look at some of the implementations.
+ *
+ * Entity models (in applications using precept) have the following characteristics:
  *   - A model can be considered a simple "business object".
  *   - One model instance can correlate with *one* entity.
  *   - An entity is the identity of the model.
- *   - The entity that the model correlates with *can* change throughout the lifetime of the model object but should
- *     only be changed internally in the implementation and only by the client via methods provided to them.
+ *   - The entity that the model correlates with *can* change throughout the lifetime of the model object.
  *   - Entities typically relate with data persistence but this is not a rule. For our purposes they're considered
  *     "objects with identities".
- *   - Models use repositories to work with the state of entities (via the repository registry used to create the
- *     instance. TODO This is actually wrong. Model repositories should have reference to models, not the other way around.
+ *   - Models can use repositories to work with the state of entities. Note that in DDD, there would be model
+ *     repositories that return models, that is, it's the other way around in larger applications but precept is just
+ *     aiming to make creating the model layer simpler and separated from the rest of the application. We sacrifice some
+ *     DDD concepts for speed in development (and because most applications I've been on haven't required more than an
+ *     architecture similar to what precept hints at).
+ *   - Models can also use event dispatchers, validators, and loggers (among other things) for internals.
  *
  * @author John Pancoast <johnpancoaster@gmail.com>
  */
 interface EntityModelInterface
 {
-    /**
-     * Create an instance of this model
-     *
-     * @param EntityModelFactoryInterface $modelFactory       The model factory creating this model
-     *
-     * @param EntityInterface|null        $entity             The entity this model is based on or null.
-     * @param RepositoryRegistryInterface $repositoryRegistry A repository registry that the model will use.
-     * @param EventDispatcherInterface    $eventDispatcher    Event dispatcher
-     *
-     * @return EntityModelInterface
-     */
-    public static function createInstance(
-        EntityModelFactoryInterface $modelFactory,
-        EntityInterface $entity = null,
-        RepositoryRegistryInterface $repositoryRegistry,
-        EventDispatcherInterface $eventDispatcher
-    );
-
     /**
      * Get entity
      *
